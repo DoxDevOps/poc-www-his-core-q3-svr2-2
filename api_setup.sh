0e50a7f9542bda9ae9a4d83b874b45395bf04883
Billz <58287516+billz94@users.noreply.github.com>
2022-06-26 11:19:19 +0200

    Create api_setup.sh

---
+++ api_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+echo "--------------------------------------------"
+echo "Checkout to latest tag"
+echo "--------------------------------------------"
+git checkout v4.15.4 -f
+echo "--------------------------------------------"
+echo "Describing Head"
+echo "--------------------------------------------"
+git describe > HEAD
+echo "____________________________________________"
+echo "Removing Gemfile.lock"
+echo "____________________________________________"
+rm Gemfile.lock
+echo "____________________________________________"
+echo "Installing Local Gems"
+echo "____________________________________________"
+bundle install --local
+echo "--------------------------------------------"
+#echo "killing sync_worker cron job"
+#pkill -f bin/lab/sync_worker.rb
+echo "____________________________________________"
+echo "running bin_update art"
+echo "____________________________________________"
+./bin/update_art_metadata.sh development
+echo "--------------------------------------------"
