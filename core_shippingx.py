9b1a15e9eee602996e2712967fa7b03d2149f184
zio-git <73478973+zio-git@users.noreply.github.com>
2022-07-11 12:12:53 +0200

    tag

--- core_shippingx.py
+++ core_shippingx.py
@@ -71,7 +71,7 @@ for site_id in cluster['site']:
             
             version = msg.format(result).strip()
             
-            core_version = "v1.2.8"
+            core_version = "v1.2.12"
             
             if core_version == version:
                 msgx = "Hi there,\n\nDeployment of HIS-Core to " + version + " for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
@@ -98,7 +98,7 @@ for site_id in cluster['site']:
             # make sure we are sending the alert at the last pint attempt
             if count == 3:
                 for recipient in recipients:
-                    msg = "Hi there,\n\nDeployment of HIS-Core to v1.2.8 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
+                    msg = "Hi there,\n\nDeployment of HIS-Core to v1.2.12 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
                     params = {
                         "api_key": os.getenv('API_KEY'),
                         "recipient": recipient,
