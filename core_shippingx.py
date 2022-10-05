c9396f1dfa5b4a5642f786a5302b754b6a2827cf
Billz <58287516+billz94@users.noreply.github.com>
2022-10-05 17:56:01 +0200

    Update core_shippingx.py

--- core_shippingx.py
+++ core_shippingx.py
@@ -34,10 +34,10 @@ def alert(url, params):
         return False
     return True
 
-recipients = ["+265998006237", "+265991450316","+265992182669", "+265995246144", "+265998276712", "+265888231289"]
+recipients = ["+265998006237", "+265991450316","+265992182669", "+265995246144", "+265998276712", "+265992508308", "+265998800628", "+265997762346", "+265996551261", "+265993030442", "+265992268777", "+265999453942", "+265995494908"]
 
 #* Get cluster details
-cluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/1')
+cluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/20')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_site/' + str(site_id))
