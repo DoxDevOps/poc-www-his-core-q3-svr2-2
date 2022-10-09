3c6fbcd9ae3b321863250a26265b6681ece404f1
Billz <58287516+billz94@users.noreply.github.com>
2022-10-09 13:00:25 +0200

    Update core_shippingx.py
    cluster 20 (v1.4.2)

--- core_shippingx.py
+++ core_shippingx.py
@@ -104,7 +104,7 @@ for site_id in cluster['site']:
             # make sure we are sending the alert at the last pint attempt
             if count == 3:
                 for recipient in recipients:
-                    msg = "Hi there,\n\nDeployment of HIS-Core to v1.4.1 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF/LIN HIS."
+                    msg = "Hi there,\n\nDeployment of HIS-Core to v1.4.2 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF/LIN HIS."
                     params = {
                         "api_key": os.getenv('API_KEY'),
                         "recipient": recipient,
