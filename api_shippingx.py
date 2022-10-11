8ab456fc438ed320577830e972ca2ced20f1eabe
Tiwonge Lwara <43346571+lwara@users.noreply.github.com>
2022-10-11 17:18:21 +0200

    Update api_shippingx.py
    40

--- api_shippingx.py
+++ api_shippingx.py
@@ -32,9 +32,9 @@ def alert(url, params):
         return False
     return True
 
-recipients = ["+265998006237", "+265991450316", "+265992182669", "+265995246144", "+265998276712", "+265995606018", "+265995594638", "+265999529559", "+265992268777", "+265993030442"]
+recipients =  ["+265998006237", "+265991450316","+265992182669", "+265995246144", "+265998276712", "+265884642428", "+265991852093", "+265991239213", "+265995532195", "+265991638843"]
 
-cluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/7')
+cluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/40')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_site/' + str(site_id))
