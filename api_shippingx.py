37b4724f49df678d855a9a0c86014568a7c584ee
zio-git <73478973+zio-git@users.noreply.github.com>
2023-01-17 14:10:50 +0200

    36

--- api_shippingx.py
+++ api_shippingx.py
@@ -32,9 +32,9 @@ def alert(url, params):
         return False
     return True
 
-recipients =  ["+265998006237", "+265995246144", "+265991450316", "+265998276712", "+265992182669", "+265992600883"]
+recipients =  ["+265998006237", "+265995246144", "+265991450316", "+265998276712", "+265992182669", "+265995440499", "+265994427700", "+265999611280", "+265995826415"]
 
-cluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/1')
+cluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/36')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_site/' + str(site_id))
